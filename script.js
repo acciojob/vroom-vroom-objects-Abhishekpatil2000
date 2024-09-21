53cf895057c67f5de052dcb5e875a251f7bab515
Abhishekpatil2000 <115855289+Abhishekpatil2000@users.noreply.github.com>
2024-09-21 13:51:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 // Coclass Car{  // parent class
-	constructor(make,model) { // constructor with two arugements.
-		this.make=make;
-		this.model=model;
-	}
-	
-  getmakeModel(){ 
-	  return `${this.make} ${this.model}`
-  }
+	class Car {
+    constructor(make, model) {
+        this.make = make;
+        this.model = model;
+    }
+
+    getMakeModel() {
+        return `${this.make} ${this.model}`;
+    }
 }
 
-class SportsCar extends Car{
-	constructor(make,model,topSpeed){
-		super(make,model)
-		this.topSpeed=topSpeed;
-	}
-	
-	getTopSpeed(){
-		return `${this.topSpeed}`
-	}
+class SportsCar extends Car {
+    constructor(make, model, topSpeed) {
+        super(make, model);
+        this.topSpeed = topSpeed;
+    }
+
+    getTopSpeed() {
+        return this.topSpeed;
+    }
 }
 
-const Mclaren=new SportsCar("US","Gt",400);
-console.log(Mclaren.getmakeModel());
+const Mclaren = new SportsCar("US", "Gt", 400);
+console.log(Mclaren.getMakeModel());
 console.log(Mclaren.getTopSpeed());
+
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
