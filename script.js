509b9f1d28228f4df1ab37a1f0871052eccc1873
Abhishekpatil2000 <115855289+Abhishekpatil2000@users.noreply.github.com>
2024-09-21 12:55:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 // Complete the js code
-function Car(make, model) {}
+class Car{  // parent class
+	constructor(make,model) { // constructor with two arugements.
+		this.make=make;
+		this.model=model;
+	}
+  getmakemodel(){ 
+	  return `${this.make} ${this.model}`
+  }
+}
 
-function SportsCar(make, model, topSpeed) {}
+class SportsCar extends Car{
+	constructor(make,model,topSpeed){
+		super(make,model,topSpeed)
+		this.topSpeed=topSpeed;
+	}
+	getTopSpeed(){
+		return `${this.topSpeed}`
+	}
+}
 
+const Mclaren=new SportsCar("US","Gt","129kms");
+Mclaren.getMakeModel;
+Mclaren.getTopSpeed;
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
